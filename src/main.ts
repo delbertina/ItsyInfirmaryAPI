a15fda8db0d9253f7701288c9d8c83e12a8f16f9
delbertina <6349928+delbertina@users.noreply.github.com>
2024-03-04 19:28:17 -0600

    Add debug statement for API port display

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const APP_PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APP_PORT);
+  console.log(`API Listening on port: ${APP_PORT}`);
 }
 bootstrap();
