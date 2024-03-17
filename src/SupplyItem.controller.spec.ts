efa15bbde346a03822fe21c5d0f93897e15708cc
delbertina <6349928+delbertina@users.noreply.github.com>
2024-03-17 17:11:17 -0500

    In progress tests, passing yet not very good

--- src/SupplyItem.controller.spec.ts
+++ src/SupplyItem.controller.spec.ts
@@ -2,36 +2,42 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SupplyItemController } from './controllers/SupplyItem.controller';
 import { STARTER_SUPPLY_ITEMS } from './data/StarterSupplyItems';
 import { SupplyItemService } from './services/SupplyItem.service';
+import { INestApplication } from '@nestjs/common';
 
-const mockSupplyItemService = jest.mock(
-  './services/SupplyItem.service.ts',
-  () =>
-    new Promise((resolve) =>
-      resolve({
-        findAll: () => Promise.resolve(STARTER_SUPPLY_ITEMS),
-        findOne: (id: number) =>
-          Promise.resolve(STARTER_SUPPLY_ITEMS.find((item) => item.id === id)),
-      }),
-    ),
-);
+// const mockSupplyItemService = jest.mock(
+//   './services/SupplyItem.service.ts',
+//   () =>
+//     new Promise((resolve) =>
+//       resolve({
+//         findAll: () => Promise.resolve(STARTER_SUPPLY_ITEMS),
+//         findOne: (id: number) =>
+//           Promise.resolve(STARTER_SUPPLY_ITEMS.find((item) => item.id === id)),
+//       }),
+//     ),
+// );
 
 describe('SupplyItemController', () => {
+  let testApp: INestApplication;
   let supplyItemController: SupplyItemController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [SupplyItemController],
       providers: [SupplyItemService],
-    })
-      .overrideProvider(SupplyItemService)
-      .useValue(mockSupplyItemService)
-      .compile();
+    }).compile();
 
+    testApp = app.createNestApplication();
     supplyItemController = app.get<SupplyItemController>(SupplyItemController);
+    await testApp.init();
   });
 
   describe('getSupplyItem', () => {
     it('should return a supply item', async () => {
+      jest
+        .spyOn(supplyItemController, 'getSupplyItem')
+        .mockImplementationOnce(
+          async (id: number) => await STARTER_SUPPLY_ITEMS[id - 1],
+        );
       expect(await supplyItemController.getSupplyItem(1)).toBe(
         STARTER_SUPPLY_ITEMS[0],
       );
