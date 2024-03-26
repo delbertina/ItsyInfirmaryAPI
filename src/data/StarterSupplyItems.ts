c7d3f50b482f603e44fc2290a04cf81ddecf0a06
delbertina <6349928+delbertina@users.noreply.github.com>
2024-03-25 21:15:32 -0500

    Add a lot more default data to prep for list tests

--- src/data/StarterSupplyItems.ts
+++ src/data/StarterSupplyItems.ts
@@ -3,20 +3,208 @@ import { SupplyItem } from 'src/models/SupplyItem.model';
 export const STARTER_SUPPLY_ITEMS: Array<SupplyItem> = [
   {
     id: 1,
-    name: 'Napkin',
-    description: 'Plain white 2-ply somewhat absorbant paper product.',
+    name: 'Cocktail Napkin',
+    description:
+      'Small plain white 2-ply somewhat absorbant paper product. Approx. the size of a coaster.',
     perishable: false,
   },
   {
     id: 2,
-    name: 'Pencil',
-    description: 'Small wooden writing untencil',
+    name: '#2 Pencil',
+    description:
+      'Small wooden writing untencil. Requires sharpening after some use.',
     perishable: false,
   },
   {
     id: 3,
-    name: 'Paper',
-    description: 'Stiff white paper product. Good for writing on.',
+    name: 'Looseleaf Paper',
+    description:
+      'Stiff white paper product. Has printed lines & is good for writing on.',
+    perishable: false,
+  },
+  {
+    id: 4,
+    name: 'Nitrile Gloves',
+    description:
+      'Slightly stretchy sterile hand coverings. Fits most sizes of hands.',
+    perishable: false,
+  },
+  {
+    id: 5,
+    name: 'Gauze Roll',
+    description:
+      'Two inch wide by 2 foot long of absorbant medical grade material.',
+    perishable: false,
+  },
+  {
+    id: 6,
+    name: 'Gauze Pad',
+    description: 'Four inch square of absorbant medical grade material.',
+    perishable: false,
+  },
+  {
+    id: 7,
+    name: 'Sergical Mask',
+    description: 'Face covering to prevent germ transmission.',
+    perishable: false,
+  },
+  {
+    id: 8,
+    name: 'Spot Bandage',
+    description: 'Small circle adhesive bandage for minor wounds.',
+    perishable: false,
+  },
+  {
+    id: 9,
+    name: 'Patch Bandage',
+    description: 'Large square adhesive bandage for large wounds.',
+    perishable: false,
+  },
+  {
+    id: 10,
+    name: 'Classic Bandage',
+    description: 'Rectangular adhesive bandage for small to medium wounds.',
+    perishable: false,
+  },
+  {
+    id: 11,
+    name: 'Antiseptic Wipe',
+    description: 'Serilizing wipes for preping wounds.',
+    perishable: false,
+  },
+  {
+    id: 12,
+    name: 'Tweezers',
+    description: 'Small metal tool for pulling on small objects.',
+    perishable: false,
+  },
+  {
+    id: 13,
+    name: 'Scissors',
+    description: 'Handheld metal tool for cutting.',
+    perishable: false,
+  },
+  {
+    id: 14,
+    name: 'Space Blanket',
+    description: 'Lightweight metalic sheet used to prevent heat loss.',
+    perishable: false,
+  },
+  {
+    id: 15,
+    name: 'Single-Sided Tape',
+    description: 'Light weight tape made for lightweight use cases.',
+    perishable: false,
+  },
+  {
+    id: 16,
+    name: 'Double-Sided Tape',
+    description: 'Light weight tape made with sticky on both sides.',
+    perishable: false,
+  },
+  {
+    id: 17,
+    name: 'Masking Tape',
+    description:
+      'Thicker tape made for being easily removable and not absorbant.',
+    perishable: false,
+  },
+  {
+    id: 18,
+    name: 'Duct Tape',
+    description: 'Heavy duty tape with embedded strengthening strings.',
+    perishable: false,
+  },
+  {
+    id: 19,
+    name: 'Small Cotton Ball',
+    description: 'Half inch diameter absorbant ball.',
+    perishable: false,
+  },
+  {
+    id: 20,
+    name: 'Large Cotton Ball',
+    description: 'Two inch diameter absorbant ball.',
+    perishable: false,
+  },
+  {
+    id: 21,
+    name: 'One-Use Hand Sanatizer',
+    description: 'Container with enough sanitizing liquid for 1 set of hands.',
+    perishable: false,
+  },
+  {
+    id: 22,
+    name: 'Small Safety Pin',
+    description: 'Fasteners for joining fabric.',
+    perishable: false,
+  },
+  {
+    id: 23,
+    name: 'Large Safety Pin',
+    description: 'Fasteners for joining fabric.',
+    perishable: false,
+  },
+  {
+    id: 24,
+    name: 'Permanent Market',
+    description: 'Writing utencile for making marks that are hard to remove.',
+    perishable: false,
+  },
+  {
+    id: 25,
+    name: 'Facial Tissue',
+    description: 'Thin paper product for cleaning up a face.',
+    perishable: false,
+  },
+  {
+    id: 26,
+    name: 'Cotton Swab',
+    description: 'Small plastic rod with packed cotton on both ends.',
+    perishable: false,
+  },
+  {
+    id: 27,
+    name: 'Popsickle Stick',
+    description: 'Ridged and flat wooden stick.',
+    perishable: false,
+  },
+  {
+    id: 28,
+    name: 'Safety Goggle',
+    description: 'OSHA certified eye protection.',
+    perishable: false,
+  },
+  {
+    id: 29,
+    name: 'Over-Glasses Safety Goggle',
+    description: 'OSHA certified eye protection to wear over glasses.',
+    perishable: false,
+  },
+  {
+    id: 30,
+    name: 'Bar Soap',
+    description:
+      'Solid brick of germ killing material usually for washing hands.',
+    perishable: false,
+  },
+  {
+    id: 31,
+    name: 'Ballpoint Pen',
+    description: 'Writing utensile with ink for writing.',
+    perishable: false,
+  },
+  {
+    id: 32,
+    name: 'Clipboard',
+    description:
+      'Solid wooden board with metal pincher for attaching paper and holding pen.',
+    perishable: false,
+  },
+  {
+    id: 33,
+    name: 'Rubber Band',
+    description: 'Stretchy rubber loop.',
     perishable: false,
   },
 ];
