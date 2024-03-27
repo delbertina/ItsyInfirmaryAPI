e2e31fdf02c45211316a710a0b20fb498f6e373f
delbertina <6349928+delbertina@users.noreply.github.com>
2024-03-26 21:08:57 -0500

    Add tests for supply item controller

--- src/SupplyItem.controller.spec.ts
+++ src/SupplyItem.controller.spec.ts
@@ -2,14 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SupplyItemController } from './controllers/SupplyItem.controller';
 import { STARTER_SUPPLY_ITEMS } from './data/StarterSupplyItems';
 import { SupplyItemService } from './services/SupplyItem.service';
-import { INestApplication } from '@nestjs/common';
+import { HttpException, INestApplication } from '@nestjs/common';
 import { SupplyItem } from './models/SupplyItem.model';
 
 const mockSupplyItemService = {
   findAll: jest
     .fn()
     .mockImplementation((take?: number, skip?: number) =>
-      STARTER_SUPPLY_ITEMS.slice(skip ?? 0, (skip ?? 0) + (take ?? 0) + 1),
+      STARTER_SUPPLY_ITEMS.slice(skip ?? 0, (skip ?? 0) + (take ?? 0)),
     ),
   findOne: jest
     .fn()
@@ -18,7 +18,7 @@ const mockSupplyItemService = {
     ),
   updateOne: jest.fn().mockResolvedValue((item: SupplyItem) => item),
   saveOne: jest.fn().mockImplementation((item: SupplyItem) => {
-    item.id = STARTER_SUPPLY_ITEMS[-1].id + 1;
+    item.id = STARTER_SUPPLY_ITEMS[STARTER_SUPPLY_ITEMS.length - 1].id + 1;
     return item;
   }),
   deleteOne: jest.fn().mockImplementation((id: number) => {
@@ -63,11 +63,69 @@ describe('SupplyItemController', () => {
     });
   });
 
+  describe('addSupplyItem', () => {
+    it('should return an error with invalid supply item with no properties', async () => {
+      expect(async () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await supplyItemController.addSupplyItem({});
+      }).rejects.toThrow(HttpException);
+    });
+  });
+
+  describe('addSupplyItem', () => {
+    it('should return an error with invalid supply item with missing property', async () => {
+      expect(async () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await supplyItemController.addSupplyItem({ id: 1, name: 'test item' });
+      }).rejects.toThrow(HttpException);
+    });
+  });
+
+  describe('addSupplyItem', () => {
+    it('should return item back when add valid item', async () => {
+      const result = await supplyItemController.addSupplyItem({
+        id: 1,
+        name: 'test item',
+        description: 'test stuff',
+        perishable: false,
+      });
+      expect(supplyItemService.saveOne).toHaveBeenCalled();
+      expect(result.id).not.toEqual(1);
+    });
+  });
+
   describe('getSupplyItemList', () => {
-    it('should return all supply items', async () => {
+    it('should return 25 supply items by default', async () => {
       const result = await supplyItemController.getSupplyItemList();
       expect(supplyItemService.findAll).toHaveBeenCalled();
-      expect(result).toHaveLength(3);
+      expect(result).toHaveLength(25);
+    });
+  });
+
+  describe('getSupplyItemList', () => {
+    it('should return 25 supply items max', async () => {
+      const result = await supplyItemController.getSupplyItemList(30);
+      expect(supplyItemService.findAll).toHaveBeenCalled();
+      expect(result).toHaveLength(25);
+    });
+  });
+
+  describe('getSupplyItemList', () => {
+    it('should return 5 supply items when take 5', async () => {
+      const result = await supplyItemController.getSupplyItemList(5);
+      expect(supplyItemService.findAll).toHaveBeenCalled();
+      expect(result).toHaveLength(5);
+    });
+  });
+
+  describe('getSupplyItemList', () => {
+    it('should return the 6th supply items when take 5 and skip 5', async () => {
+      const result = await supplyItemController.getSupplyItemList(5, 5);
+      expect(supplyItemService.findAll).toHaveBeenCalled();
+      expect(result).toHaveLength(5);
+      expect(result[0].id).toEqual(6);
     });
   });
 });
